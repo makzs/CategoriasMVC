88a7fe952d7934364740d9b8d96c4ce1b890b9ba
makzs <erikmakiak@gmail.com>
2024-10-03 18:06:53 -0300

    implementando view do get all

---
+++ CategoriasMVC/Controllers/CategoriasController.cs
@@ -0,0 +1,26 @@
+﻿using CategoriasMVC.Models;
+using CategoriasMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CategoriasMVC.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly ICategoriaService _categoriaService;
+
+        public CategoriasController(ICategoriaService categoriaService)
+        {
+            _categoriaService = categoriaService;
+        }
+
+        public async Task<ActionResult<IEnumerable<CategoriaViewModel>>> Index()
+        {
+            var result = await _categoriaService.GetCategorias();
+
+            if (result == null)
+                return View("error");
+
+            return View(result);
+        }
+    }
+}

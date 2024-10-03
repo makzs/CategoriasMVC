1ab355c4c5ea06ae32267c678939c0ef4574d413
makzs <erikmakiak@gmail.com>
2024-10-03 16:30:49 -0300

    criando metodos do CategoriaService

--- CategoriasMVC/Services/CategoriaService.cs
+++ CategoriasMVC/Services/CategoriaService.cs
@@ -1,4 +1,5 @@
 ﻿using CategoriasMVC.Models;
+using System.Text;
 using System.Text.Json;
 
 namespace CategoriasMVC.Services
@@ -20,27 +21,98 @@ namespace CategoriasMVC.Services
 
         public async Task<bool> AtualizaCategoria(int id, CategoriaViewModel categoriaVM)
         {
-            throw new NotImplementedException();
+            var client = _clientFactory.CreateClient("CategoriasApi");
+            using (var response = await client.PutAsJsonAsync(apiEndpoint + id, categoriaVM))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
 
         public async Task<CategoriaViewModel> CriaCategoria(CategoriaViewModel categoriaVM)
         {
-            throw new NotImplementedException();
+            var client = _clientFactory.CreateClient("CategoriasApi");
+            var categoria = JsonSerializer.Serialize(categoriaVM);
+            StringContent content = new StringContent(categoria, Encoding.UTF8, "application/json");
+            using (var response = await client.PostAsync(apiEndpoint, content))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var apiResponse = await response.Content.ReadAsStreamAsync();
+                    categoriaVM = await JsonSerializer
+                        .DeserializeAsync<CategoriaViewModel>
+                        (apiResponse, _options);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            return categoriaVM;
         }
 
         public async Task<bool> DeletaCategoria(int id)
         {
-            throw new NotImplementedException();
+            var client = _clientFactory.CreateClient("CategoriasApi");
+            using (var response = await client.DeleteAsync(apiEndpoint + id))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
 
         public async Task<CategoriaViewModel> GetCategoriaPorId(int id)
         {
-            throw new NotImplementedException();
+            var client = _clientFactory.CreateClient("CategoriasApi");
+            using (var response = await client.GetAsync(apiEndpoint + id))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var apiResponse = await response.Content.ReadAsStreamAsync();
+                    categoriaVM = await JsonSerializer
+                        .DeserializeAsync<CategoriaViewModel>
+                        (apiResponse, _options);
+
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            return categoriaVM;
         }
 
         public async Task<IEnumerable<CategoriaViewModel>> GetCategorias()
         {
-            throw new NotImplementedException();
+            var client = _clientFactory.CreateClient("CategoriasApi");
+            using (var response = await client.GetAsync(apiEndpoint))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var apiResponse = await response.Content.ReadAsStreamAsync();
+                    categoriasVM = await JsonSerializer
+                        .DeserializeAsync<IEnumerable<CategoriaViewModel>>
+                        (apiResponse, _options);
+
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            return categoriasVM;
         }
     }
 }
